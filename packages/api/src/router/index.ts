09f8d09926288272fb582161d758e6ebc6edf381
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2023-05-10 22:53:29 -0600

    feat(api): add people router

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,10 +1,12 @@
 import { router } from '../trpc';
 import { authRouter } from './auth';
 import { familiesRouter } from './families';
+import { peopleRouter } from './people';
 
 export const appRouter = router({
-  auth: authRouter,
   families: familiesRouter,
+  people: peopleRouter,
+  auth: authRouter,
 });
 
 // export type definition of API
---
+++ packages/api/src/router/people.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod';
+import { publicProcedure, router } from '../trpc';
+import { PersonRole } from '@acme/db';
+
+export const peopleRouter = router({
+  create: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        role: z.enum([
+          PersonRole.BEST_MAN,
+          PersonRole.BRIDESMAID,
+          PersonRole.GROOMSMAN,
+          PersonRole.MAID_OF_HONOR,
+          PersonRole.BRIDES_FATHER,
+          PersonRole.BRIDES_MOTHER,
+          PersonRole.GROOMS_FATHER,
+          PersonRole.GROOMS_MOTHER,
+          PersonRole.FLOWER_GIRL,
+          PersonRole.RING_BEARER,
+          PersonRole.NONE,
+        ]),
+        isChildren: z.boolean(),
+        familyId: z.string(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.person.create({
+        data: input,
+        include: {
+          family: true,
+        },
+      });
+    }),
+  list: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.person.findMany({
+      include: {
+        family: {
+          include: {
+            invitation: true,
+          },
+        },
+      },
+    });
+  }),
+});
