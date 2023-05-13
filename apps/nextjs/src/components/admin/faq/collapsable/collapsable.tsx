eae85eab33aeb938400e1bfd7846a3e4358bbc5f
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2023-05-13 09:38:26 -0600

    feat(nextjs): ui to crud faqs

--- apps/nextjs/src/components/admin/faq/collapsable/collapsable.tsx
+++ apps/nextjs/src/components/admin/faq/collapsable/collapsable.tsx
@@ -1,16 +1,111 @@
-import type { FC } from 'react';
+import { useState, type FC, useMemo } from 'react';
+import {
+  FaBan,
+  FaCheck,
+  FaChevronDown,
+  FaChevronUp,
+  FaPen,
+  FaTrash,
+} from 'react-icons/fa';
 
-export const AdminFaqCollapsable: FC = () => {
+type FaqData = {
+  question: string;
+  answer: string;
+};
+
+export type AdminFaqCollapsableProps = FaqData & {
+  onEdit: (data: FaqData) => void;
+  onDelete: () => void;
+};
+
+export const AdminFaqCollapsable: FC<AdminFaqCollapsableProps> = ({
+  question,
+  answer,
+  onEdit,
+  onDelete,
+}) => {
+  const [mutation, setMutation] = useState<'edit' | 'delete'>();
+  const [open, setOpen] = useState<boolean>(false);
+  const [questionValue, setQuestionValue] = useState<string>(question);
+  const [answerValue, setAnswerValue] = useState<string>(answer);
+
+  const className = useMemo(
+    () => (open ? 'collapse-open' : 'collapse-close'),
+    [open],
+  );
+
+  const toggle = () => setOpen((prev) => !prev);
+
+  const confirm = () => {
+    if (!mutation) return;
+    if (mutation === 'edit') {
+      onEdit({
+        question: questionValue,
+        answer: answerValue,
+      });
+    } else if (mutation === 'delete') {
+      onDelete();
+    }
+    setMutation(undefined);
+  };
+
   return (
     <div
-      tabIndex={0}
-      className="collapse-arrow rounded-box collapse border border-base-300 bg-base-100"
+      className={`rounded-box collapse border border-base-300 bg-base-100 ${className}`}
     >
-      <div className="collapse-title text-xl font-medium">
-        Focus me to see content
+      <button
+        className="btn-ghost btn-xs btn absolute right-2 top-5 z-10"
+        onClick={toggle}
+      >
+        {open ? <FaChevronUp size={16} /> : <FaChevronDown size={16} />}
+      </button>
+      <div className="collapse-title flex w-full items-center">
+        <div className="flex-1">
+          <input
+            disabled={mutation !== 'edit'}
+            className="input-bordered input input-sm w-11/12 text-lg font-medium"
+            value={questionValue}
+            onChange={(e) => setQuestionValue(e.target.value)}
+          />
+        </div>
+        <div className="inline-flex flex-none gap-2">
+          {mutation ? (
+            <>
+              <button
+                onClick={() => setMutation(undefined)}
+                className="btn-ghost btn-xs btn"
+              >
+                <FaBan />
+              </button>
+              <button onClick={confirm} className="btn-ghost btn-xs btn">
+                <FaCheck />
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => setMutation('edit')}
+                className="btn-ghost btn-xs btn"
+              >
+                <FaPen className="text-blue-500" />
+              </button>
+              <button
+                onClick={() => setMutation('delete')}
+                className="btn-ghost btn-xs btn"
+              >
+                <FaTrash className="text-red-500" />
+              </button>
+            </>
+          )}
+        </div>
       </div>
       <div className="collapse-content">
-        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        <textarea
+          disabled={mutation !== 'edit'}
+          className="textarea-bordered textarea w-11/12"
+          value={answerValue}
+          onChange={(e) => setAnswerValue(e.target.value)}
+        />
       </div>
     </div>
   );
--- apps/nextjs/src/pages/admin/faq/index.tsx
+++ apps/nextjs/src/pages/admin/faq/index.tsx
@@ -3,6 +3,8 @@ import type { NextPage } from 'next';
 import { IBreadcrumb } from '../../../components/admin/header/breadcrumbs';
 import { AdminHeader } from '../../../components/admin/header';
 import { trpc } from '../../../utils/trpc';
+import { AdminFaqCollapsable } from '../../../components/admin/faq/collapsable';
+import { FaPlus } from 'react-icons/fa';
 
 const breadcrumbs: IBreadcrumb[] = [
   {
@@ -13,7 +15,26 @@ const breadcrumbs: IBreadcrumb[] = [
 ];
 
 const AdminFamilies: NextPage = () => {
-  const { data } = trpc.faq.list.useQuery();
+  const { data, refetch } = trpc.faq.list.useQuery();
+
+  const createFaq = trpc.faq.create.useMutation({
+    onSuccess: () => refetch(),
+  });
+
+  const updateFaq = trpc.faq.update.useMutation({
+    onSuccess: () => refetch(),
+  });
+
+  const deleteFaq = trpc.faq.delete.useMutation({
+    onSuccess: () => refetch(),
+  });
+
+  const createEmptyFaq = () => {
+    createFaq.mutate({
+      question: '',
+      answer: '',
+    });
+  };
 
   return (
     <>
@@ -27,6 +48,20 @@ const AdminFamilies: NextPage = () => {
           <h1 className="prose text-xl font-bold">
             Frequently Asked Questions
           </h1>
+          <div className="mt-10 flex w-11/12 flex-col gap-5">
+            {data?.map((faq) => (
+              <AdminFaqCollapsable
+                {...faq}
+                key={faq.id}
+                onEdit={(data) => updateFaq.mutate({ id: faq.id, data })}
+                onDelete={() => deleteFaq.mutate({ id: faq.id })}
+              />
+            ))}
+            <button onClick={createEmptyFaq} className="btn-outline btn gap-2">
+              <FaPlus />
+              <span>Add new question</span>
+            </button>
+          </div>
         </main>
       </>
     </>
